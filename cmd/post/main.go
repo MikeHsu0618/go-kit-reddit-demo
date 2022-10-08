84be0cc8ba5f02d993cebb362d3b5b8ddc185f37
mike <a3698521tw11@gmail.com>
2022-10-08 12:36:08 +0800

    refactor: modify post file structure

---
+++ cmd/post/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"go-kit-reddit-demo/cmd/post/service"
+)
+
+func main() {
+	service.Run()
+}
--- internal/post/cmd/main.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import service "go-kit-reddit-demo/internal/post/cmd/service"
-
-func main() {
-	service.Run()
-}
